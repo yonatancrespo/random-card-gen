83bcf9d7b81b6d4a48b384032faa031c7d2d0886
yonatancrespo <yonatancrespo4@gmail.com>
2023-01-07 05:21:55 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
+import "bootstrap";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function() {};
+const values = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+const suits = ["&hearts;", "&spades;", "&clubs;", "&diams;"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+function deal() {
+  let value = values[Math.floor(Math.random() * values.length)];
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+  document.querySelector(".card-value").innerHTML = value;
+  document.querySelector(".card-suit").innerHTML = suit;
+  document.querySelector("#deal-button").addEventListener("click", deal);
+}
+
+deal();
